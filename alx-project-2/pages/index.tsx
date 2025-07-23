f2ddd7989546084b716dfcf3f15232d40ad47e2d
Dorna <dorna.imani943@gmail.com>
2025-07-23 20:13:28 +0300

    Project Set up

---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Welcome : React.FC = () => {
+  return(
+    <div className="text-center">
+      <div className="text-5xl font-bold text-white">
+        Welcome to The AirBnB Clone!
+      </div>
+    </div>
+
+  )
+}
+
+export default Welcome;
