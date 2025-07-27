9feb93b681c859bbd75c4a38a3b1ce8f040de19a
Dorna <dorna.imani943@gmail.com>
2025-07-27 22:12:14 +0300

    Implementation navigation

--- alx-project-0x02/components/layout/header.tsx
+++ alx-project-0x02/components/layout/header.tsx
@@ -4,11 +4,8 @@ import Link from "next/link";
 const Header: React.FC = () => {
     return(
         <header className="bg-white-400 text-white py-4">
-            <div className="container mx-auto flex justify-between items-center px-4">
-                <h3 className="font-bold text-2xl">
-                    Home
-                </h3>
-                <nav>
+            <nav className="container mx-auto flex justify-between items-center px-4">
+                <h3 className="font-bold text-2xl">Home</h3>
                     <ul className="flex space-x-4">
                         <li className="hover:underline">
                             <Link href="/home">Home</Link>
@@ -16,9 +13,11 @@ const Header: React.FC = () => {
                         <li className="hover:underline">
                             <Link href="/about">About</Link>
                         </li>
+                        <li className="hover:underline">
+                            <Link href="/posts">Posts</Link>
+                        </li>
                     </ul>
-                </nav>
-            </div>
+            </nav>
         </header>
     );
 }
---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "@/components/layout/Header";
+
+const Posts : React.FC = () => {
+    return(
+        <div className="text-center">
+            <Header />
+            <h1 className="text-shadow-amber-400">This is the Posts page</h1>
+        </div>
+    )
+}
+ export default Posts;
