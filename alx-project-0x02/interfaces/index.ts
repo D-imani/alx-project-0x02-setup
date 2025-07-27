6b0d05bcaa229e9332433e1038973c4b41fe74df
Dorna <dorna.imani943@gmail.com>
2025-07-27 21:59:21 +0300

    Button component

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -1,6 +1,13 @@
 //Contains the exports
 export interface CardProps{
-    title: string;
-    content : string;
+    title: string ;
+    content : string ;
 }
 
+export interface ButtonProps {
+  label: string;
+  size?: "small" | "medium" | "large";
+  shape?: "rounded-sm" | "rounded-md" | "rounded-full";
+  onClick?: () => void;
+  className?: string; // Optional for extra styling
+}
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Header from "@/components/layout/Header";
+import Button from "@/components/common/Button";
 
 const About : React.FC = () => {
     return (
         <div className="text-center">
             <Header />
             <h1 className="text-2xl">
-                An AirBnB clone project by Dorna
+                About Page
             </h1>
-            </div>
+             <Button label="Small + Rounded-sm" size="small" shape="rounded-sm" />
+            <Button label="Medium + Rounded-md" size="medium" shape="rounded-md" />
+            <Button label="Large + Rounded-full" size="large" shape="rounded-full" />
+        </div>
     )
 }
 export default About;
